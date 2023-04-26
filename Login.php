701f0981f6542e5d70b671a6ece1ee17c489addd
CameronLove21 <50743831+CameronLove21@users.noreply.github.com>
2023-04-26 14:38:15 +0100

    PHP

--- Login.php
+++ Login.php
@@ -2,22 +2,18 @@
     include_once 'header.php';
 ?>
 
+<body>
     <!--Markup all web page content inside the 'body' tags -->
     <div class="center">
         <h1>Login</h1>
         <form action="login.inc.php" method="post">
             <div class="txt_field" id="First_Name">
-                <input type="text" required>
-                <span></span>
-                <label class="email">Email</label>
+                <input type="text" name="name" placeholder="Username/Email" required>
             </div>
             <div class="txt_field">
-                <input type="password" required>
-                <span></span>
-                <label>Password</label>
+                <input type="password" name="pwd" placeholder="Password" required>
             </div>
-            <div class="pass">Forgot Password?</div>
-            <input type="submit" value="Login">
+            <button type="submit" name="Submit">Log In</button>
             <div class="signup_link">
                 Not a member? <a href="register.html">Sign Up</a>
             </div>
