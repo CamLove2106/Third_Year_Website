affd2847a37800a023642734e8b623c522c2e356
CameronLove21 <50743831+CameronLove21@users.noreply.github.com>
2023-04-26 11:45:10 +0100

    Header.php

---
+++ header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link href="grid.css" rel="stylesheet" />
+    <link href="http://fonts.googleapis.com/css?family=Strait" rel="stylesheet" />
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+    <title>Final Year Project</title>
+</head>
+<body>
+    <a id="top"></a>
+    <div>
+        <nav>
+            <input type="checkbox" id="check" />
+            <label for="check" class="checkbtn">
+                <i class="fas fa-bars"></i>
+            </label>
+            <label class="logo">Bravo 6 Recruiters</label>
+            <ul>
+                <li><a class="active" href="index.php">Home</a></li>
+                <li><a class="active" href="#">Candidates</a></li>
+                <li><a class="active" href="#">Contact</a></li>
+                <li><a class="active" href="#"></a></li>
+                <li><a class="active" href="Login.php">Log In</a></li>
+            </ul>
+        </nav>
