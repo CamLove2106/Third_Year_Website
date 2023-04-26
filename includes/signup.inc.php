25f4c301d00cd7cc78855e176e1312c8b7053fbe
CameronLove21 <50743831+CameronLove21@users.noreply.github.com>
2023-04-26 15:44:45 +0100

    functions

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -1,8 +1,43 @@
 <?php
 
 if (isset($_POST["submit"])) {
-    echo "It works";
+    
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $username = $_POST["uid"];
+    $pwd = $_POST["pwd"];
+    $pwdRepeat = $_POST["pwdrepeat"];
+
+    require_once 'dbh.inc.php';
+    require_once 'functions.inc.php';
+
+    if (emptyInputSignup($name, $email, $username, $pwd, $pwdRepeat) !== false) {
+        header("location: ../Signup.php?error=emptyinput");
+        exit();
+    }
+
+    if (invalidUid($username) !== false) {
+        header("location: ../Signup.php?error=invaliduid");
+        exit();
+    }
+
+    if (invalidEmail($email) !== false) {
+        header("location: ../Signup.php?error=invalidemail");
+        exit();
+    }
+
+    if (pwdMatch($pwd, $pwdRepeat) !== false) {
+        header("location: ../Signup.php?error=passworddontmatch");
+        exit();
+    }
+
+    if (uidExists($conn, $username) !== false) {
+        header("location: ../Signup.php?error=usernametaken");
+        exit();
+    }
+
 }
 else {
     header("location: ../Signup.php");
+    exit();
 }
