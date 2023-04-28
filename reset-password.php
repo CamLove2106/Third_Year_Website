7787b731e90ec7cff2f0681b6e8a860f3b3236e9
CameronLove21 <50743831+CameronLove21@users.noreply.github.com>
2023-04-28 14:20:14 +0100

    Reset Password Code

--- reset-password.php
+++ reset-password.php
@@ -4,39 +4,22 @@
 <body>
     <div class="center">
         <h1>Reset your password</h1>
-        <p>AN email will be sent to you with the instructoins on how to reset your password!</p>
+        <p>An email will be sent to you with the instructoins on how to reset your password!</p>
         <form action="includes/reset-request.inc.php" method="post">
             <div class="txt_field">
                 <input type="text" name="email" placeholder="Enter your email address!" required>
-                <button type="submit" name="reset-request-submit">Receieve email password reset</button>
             </div>
+            <button type="submit" name="reset-request-submit">Receieve email password reset</button>
         </form>
     </div>
 
     <?php
-        if (isset($_GET["error"])) {
-            if ($_GET["error"] == "emptyinput") {
-                echo "<p> Fill in all the fields!</p>";
-            }
-            else if ($_GET["error"] == "invaliduid") {
-                echo "<p> Choose a proper username!</p>";
-            }
-            else if ($_GET["error"] == "invalidemail") {
-                echo "<p> Choose a proper email address!</p>";
-            }
-            else if ($_GET["error"] == "passwordsdontmatch") {
-                echo "<p>Passwords don't match!</p>";
-            }
-            else if ($_GET["error"] == "stmtfailed") {
-                echo "<p>Something went wrong, Try again!</p>";
-            }
-            else if ($_GET["error"] == "usernametaken") {
-                echo "<p>Username already taken!</p>";
-            }
-            else if ($_GET["error"] == "none") {
-                echo "<p>You have signed up!</p>";
-            }
+        if (isset($_GET["reset"])) {
+            if ($_GET["reset"] == "success") {
+                echo '<p class="signupsuccess">Check your email!</p>';
         }
+
+    }
     ?>
 
 <?php
