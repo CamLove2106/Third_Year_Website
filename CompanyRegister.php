e73435fe079d1bccfbfca22c002c3fadb89d11e0
CameronLove21 <50743831+CameronLove21@users.noreply.github.com>
2023-04-29 13:18:15 +0100

    company register

--- CompanyRegister.php
+++ CompanyRegister.php
@@ -1,26 +1,6 @@
-<!doctype html>
-<!--HTML5 doctype declaration -->
-<html lang="en">
-<!--default language of the document content -->
-
-<head>
-    <meta charset="utf-8"><!--character encoding for the document (Unicode) -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Registeration Page</title><!--web page title -->
-    <link href="Login_page.css" rel="stylesheet">
-    <link href="http://fonts.googleapis.com/css?family=Strait" rel="stylesheet">
-
-    <style>
-        body {
-            margin: 0;
-            padding: 0;
-            font-family: "Helvetica Neue", Helvetica, Arial, Geneva, sans-serif;
-            font-size: 100%;
-            background-color: #fff;
-        }
-    </style>
-
-</head>
+<?php
+    include_once 'header.php';
+?>
 <body>
     <div class="center">
         <h1>Registration</h1>
@@ -65,14 +45,6 @@
 
 
 
-    <footer>
-        <!--Footer content -->
-        <nav class="footer">
-            <small>&copy; 2023, Cameron Love. <a href="mailto:C.love1@uni.brighton.ac.uk">Email Cameron Love</a></small>
-        </nav>
-    </footer>
-
-</body><!--Close 'body' -->
-
-
-</html><!--Close 'html' -->
+<?php
+    include_once 'footer.php';
+?>
